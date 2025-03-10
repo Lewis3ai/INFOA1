5d288f987f4b60e1dd800936d777e560d8778710
Lewis3ai <158581435+Lewis3ai@users.noreply.github.com>
2025-03-10 17:21:10 +0000

    update

--- App/app.py
+++ App/app.py
@@ -58,11 +58,14 @@ def delete_pokemon():
     db.session.commit()
     return jsonify({"message": f"{user_pokemon.name} released"})
 
+
+@app.route('/')
+def index():
+    return jsonify({"message": "Welcome to the Pokemon API!"})
+
+
+
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
-
-
-if __name__ == '__main__':
-     app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
